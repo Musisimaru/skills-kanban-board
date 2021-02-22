346bb9c1f55c12bca248c80acc2abe123e34871e
Волобуев Петр <musisimaru@yandex.ru>
2021-02-22 21:43:56 +0300

    add drag and drop

--- src/components/taskboard-item-component.ts
+++ src/components/taskboard-item-component.ts
@@ -1,4 +1,5 @@
 import Task from "models/task";
+import { getNextElement } from "utils";
 import { UIComponent } from "./ui-component";
 
 export class TaskboardItemComponent extends UIComponent {
@@ -7,11 +8,13 @@ export class TaskboardItemComponent extends UIComponent {
   constructor(task: Task) {
     super();
     this._data = task;
+
+    this.initEventHandlers();
   }
 
   _getTemplate(): string {
     return `
-    <div class="taskboard__item task">
+    <div class="taskboard__item task" draggable="true">
         <div class="task__body">
             <p class="task__view">${this._data.name}</p>
             <input class="task__input" type="text" value="${this._data.name}">
@@ -20,4 +23,37 @@ export class TaskboardItemComponent extends UIComponent {
     </div>`;
   }
 
+  initEventHandlers() {
+    (<HTMLElement>this.element).addEventListener(`dragstart`, function (evt) {
+      this.classList.add(`task--dragged`);
+    });
+
+    (<HTMLElement>this.element).addEventListener(`dragend`, function (evt) {
+      this.classList.remove(`task--dragged`);
+    });
+
+    (<HTMLElement>this.element).addEventListener(`dragover`, function (evt) {
+      evt.preventDefault();
+
+      const activeElement = document.querySelector(`.taskboard__item.task--dragged`);
+      const isMoveable =
+        activeElement !== this && this.classList.contains(`taskboard__item`);
+
+      if (!isMoveable) {
+        return;
+      }
+
+      const nextElement = getNextElement(evt.clientY, this);
+
+      if (
+        (nextElement && activeElement === nextElement.previousElementSibling) ||
+        activeElement === nextElement
+      ) {
+        return;
+      }
+
+      const tasksListElement = this.parentElement;
+      tasksListElement.insertBefore(activeElement, nextElement);
+    });
+  }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,21 @@
-export function createElement(template): Element {
+export const createElement = (template): Element => {
   const element = document.createElement(`div`);
   element.innerHTML = template;
 
   return element.firstElementChild;
 }
+
+export const getNextElement = (cursorPosition: number, currentElement: Element): Element => {
+  // Получаем объект с размерами и координатами
+  const currentElementCoord = currentElement.getBoundingClientRect();
+  // Находим вертикальную координату центра текущего элемента
+  const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+
+  // Если курсор выше центра элемента, возвращаем текущий элемент
+  // В ином случае — следующий DOM-элемент
+  const nextElement = (cursorPosition < currentElementCenter) ?
+      currentElement :
+      currentElement.nextElementSibling;
+
+  return nextElement;
+};
