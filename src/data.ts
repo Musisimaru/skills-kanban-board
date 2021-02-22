a922de15aeeeb2d73f1b2500ccf366f003a41326
Волобуев Петр <musisimaru@yandex.ru>
2021-02-22 21:42:40 +0300

    more visual data

--- src/data.ts
+++ src/data.ts
@@ -2,15 +2,15 @@ import TasksBase from "models/tasksBase";
 
 export const tasks: TasksBase = {
   backlog: [
-    { name: "Test Task" },
-    { name: "Test Task" },
-    { name: "Test Task" },
+    { name: "Test Task 0-1" },
+    { name: "Test Task 0-2" },
+    { name: "Test Task 0-3" },
   ],
   processing: [
-    { name: "Test Task" },
-    { name: "Test Task" },
-    { name: "Test Task" },
+    { name: "Test Task 1-1" },
+    { name: "Test Task 1-2" },
+    { name: "Test Task 1-3" },
   ],
   done: [],
-  basket: [{ name: "Test Task" }],
+  basket: [{ name: "Test Task 3-1" }],
 };
